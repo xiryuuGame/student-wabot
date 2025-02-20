e91633e55b7d81087137e84cd8ce8461d982f7fb
xiryuuGame <farrel2008ganteng@gmail.com>
2025-02-20 14:25:00 +0700

    make a install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create temporary directory
+mkdir -p ./temp
+
+# Install npm dependencies
+npm i
+
+# Create directories for subjects
+for mapel in "AIJ" "ASJ" "B_INDO" "B_INGGRIS" "MTK" "MANDARIN" "PAI" "PJOK" "PKN" "RPL" "SEJARAH" "TLJ-PKK" "WAN"; do
+  mkdir -p "./function/mapel/$mapel"
+done
