69a7cca9aab60344ed551e33ef6b0385f456a039
xiryuuGame <farrel2008ganteng@gmail.com>
2025-03-02 06:48:57 +0700

    set the password because personal information

--- function/jadwalpiket.js
+++ function/jadwalpiket.js
@@ -2,8 +2,15 @@ import fs from 'fs'
 
 const jadwalpiket = (msg, sock) => {
     try {
+        const password = 'hahahihi'
+        const msg = msg.message.conversation || msg.message.extendedTextMessage.text || msg.message.imageMessage.caption || msg.message.videoMessage.caption || msg.message.documentWithCaptionMessage.message.documentMessage.caption || msg.message.buttonsResponseMessage.selectedButtonId || msg.message.listResponseMessage.singleSelectReply.selectedRowId
+        if(msg.includes(password)){
+            const response = 'kamu harus memberi password, dikarenakan ini berisi banyak data pribadi seseorang.'
+            sock.sendMessage(msg.key.remoteJid, {text: response}, {quoted: msg})
+            return;
+        }
+
         const response = fs.readFileSync('piket.txt', 'utf-8');
-        
 
         sock.sendMessage(msg.key.remoteJid, {text: response}, {quoted: msg})
     } catch (error) {
