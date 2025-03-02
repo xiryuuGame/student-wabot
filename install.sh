b978cf4ee58b47d770f9ed2371e3f0971ce91d5d
xiryuuGame <farrel2008ganteng@gmail.com>
2025-03-02 13:49:14 +0700

    made a question installing pm2

--- install.sh
+++ install.sh
@@ -19,3 +19,20 @@ npm i
 
 # Create .env file with GEMINI_API_KEY
 echo "GEMINI_API_KEY=*****" > .env
+
+# Ask user if they want to install pm2
+while true; do
+  read -r -p "Do you want to install pm2 globally? [y/n] (default: y) " install_pm2
+  case "$install_pm2" in
+    ""|[yY])
+      npm install -g pm2
+      break
+      ;;
+    [nN])
+      break
+      ;;
+    *)
+      echo "Invalid input. Please answer 'y' or 'n'."
+      ;;
+  esac
+done
