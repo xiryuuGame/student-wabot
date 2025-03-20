9a4cd30e1837d77b6df8c4874ec2bc2765b956b4
xiryuuGame <farrel2008ganteng@gmail.com>
2025-03-20 21:17:26 +0700

    add npm update so the library is uptodate

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 mkdir -p ./temp
 mkdir -p ./AIHistory
 
-
 # Extract unique subjects from mapel.json
 subjects=$(jq -r 'values | flatten | unique | .[]' mapel.json)
 
@@ -15,24 +14,25 @@ for mapel in $subjects; do
 done
 
 # Install npm dependencies
-npm i
+npm install
+npm update
 
 # Create .env file with GEMINI_API_KEY
-echo "GEMINI_API_KEY=*****" > .env
+echo "GEMINI_API_KEY=*****" >.env
 
 # Ask user if they want to install pm2
 while true; do
   read -r -p "Do you want to install pm2 globally? [y/n] (default: y) " install_pm2
   case "$install_pm2" in
-    ""|[yY])
-      npm install -g pm2
-      break
-      ;;
-    [nN])
-      break
-      ;;
-    *)
-      echo "Invalid input. Please answer 'y' or 'n'."
-      ;;
+  "" | [yY])
+    npm install -g pm2
+    break
+    ;;
+  [nN])
+    break
+    ;;
+  *)
+    echo "Invalid input. Please answer 'y' or 'n'."
+    ;;
   esac
 done
